e2a0fac46754c36bc53e94668807f79e54ef7ab6
Tim Brayshaw <tim@brayshaw.com>
2016-02-01 20:51:21 +0000

    An ounce of flesh.

--- build/test.js
+++ build/test.js
@@ -1,7 +1,7 @@
 import store from '../src/main.js';
 
 /**
- * For now, just log on change.
+ * For now, just log the state on change.
  */
 store.subscribe(() => {
     console.log(store.getState());
@@ -11,3 +11,8 @@ store.subscribe(() => {
  * Do something.
  */
 store.dispatch({type: 'ADD_PRODUCT', id: 'a', quantity: 6});
+
+// It shouldn't be possible to add more multiple lines with the same product id.
+store.dispatch({type: 'ADD_PRODUCT', id: 'a', quantity: 6});
+
+store.dispatch({type: 'ADD_PRODUCT', id: 'b', quantity: 12});
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -2,13 +2,25 @@
  * Define an initial state.
  */
 const initialState = {
-  addedIds: [],
+  lineIds: [],
   quantityById: {}
 }
 
 /**
- * Just a stub noop reducer.
+ * Just a stub reducer.
  */
 export default function cart(state = initialState, action) {
-    return state;
+    if (action.type === 'ADD_PRODUCT') {
+        let newState = Object.assign({}, state);
+        // Prevent adding the same id more than once.
+        if (newState.lineIds.indexOf(action.id) !== -1) {
+            // A "do nothing" strategy.
+            return state;
+        }
+        newState.lineIds.push(action.id);
+        newState.quantityById[action.id] = action.quantity;
+        return newState;
+    } else {
+        return state;
+    }
 }
